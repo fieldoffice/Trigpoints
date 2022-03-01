93ce33444b2324c1518593ca3718eaf1d93c817e
Michael Dales <michael@digitalflapjack.com>
2022-03-01 09:55:57 +0000

    Add UI test suite to use new abstraction.

--- Trigpoints/Models/Location/LocationModel.swift
+++ Trigpoints/Models/Location/LocationModel.swift
@@ -9,17 +9,37 @@ import Foundation
 import CoreLocation
 import Combine
 
-class AnyLocationModel: ObservableObject {
-    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
-    @Published var approximateLocation: CLLocation?
+protocol AbstractLocationModel {
     
-    private let locationModel: LocationModel
+    var approximateLocationPublisher: Published<CLLocation?>.Publisher { get }
+    
+    var authorizationStatusPublisher: Published<CLAuthorizationStatus>.Publisher { get }
+    
+    func requestPermission()
+}
+
+// This is a work around for two limitations in SwiftUI for UI test automation:
+//   1: You can't use protocols for StateObjects or for @Published methods, so this
+//      class is providing an abstraction layer for that.
+//   2: You can't provide constructors to StateObjects, which is why you need to switch
+//      between the mock and real objects here, rather than taking locationModel as a
+//      parameter.
+// It's all a bit icky, but so far I've not seen a better way to enable me to do UI
+// testing when I have location dependancies for my data.
+
+let kUITestingFlag = "-isUItesting"
+
+class AnyLocationModel: ObservableObject {
+    @Published private(set) var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    @Published private(set) var approximateLocation: CLLocation?
+    
+    private let locationModel: AbstractLocationModel
     var cancellables = Set<AnyCancellable>()
     
     init() {
-        locationModel = LocationModel()
-        locationModel.$approximateLocation.sink(receiveValue: {self.approximateLocation = $0}).store(in: &cancellables)
-        locationModel.$authorizationStatus.sink(receiveValue: {self.authorizationStatus = $0}).store(in: &cancellables)
+        locationModel = ProcessInfo.processInfo.arguments.contains(kUITestingFlag) ? MockLocationModel() : LocationModel()
+        locationModel.approximateLocationPublisher.sink(receiveValue: {self.approximateLocation = $0}).store(in: &cancellables)
+        locationModel.authorizationStatusPublisher.sink(receiveValue: {self.authorizationStatus = $0}).store(in: &cancellables)
     }
     
     func requestPermission() {
@@ -27,11 +47,40 @@ class AnyLocationModel: ObservableObject {
     }
 }
 
-class LocationModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    @Published var authorizationStatus: CLAuthorizationStatus
+class MockLocationModel: ObservableObject, AbstractLocationModel {
+    @Published private(set) var authorizationStatus: CLAuthorizationStatus
+    var authorizationStatusPublisher: Published<CLAuthorizationStatus>.Publisher {
+        return $authorizationStatus
+    }
+    
     // This only updates if we've moved a reasonable distance, to try
     // reduce UI updates as the GPS bounces
-    @Published var approximateLocation: CLLocation?
+    @Published private(set) var approximateLocation: CLLocation?
+    var approximateLocationPublisher: Published<CLLocation?>.Publisher {
+        return $approximateLocation
+    }
+    
+    init() {
+        authorizationStatus = .authorizedWhenInUse
+        approximateLocation = CLLocation(latitude: 55.8, longitude: -4.2)
+    }
+    
+    func requestPermission() {
+    }
+}
+
+class LocationModel: NSObject, ObservableObject, CLLocationManagerDelegate, AbstractLocationModel {
+    @Published private(set) var authorizationStatus: CLAuthorizationStatus
+    var authorizationStatusPublisher: Published<CLAuthorizationStatus>.Publisher {
+        return $authorizationStatus
+    }
+    
+    // This only updates if we've moved a reasonable distance, to try
+    // reduce UI updates as the GPS bounces
+    @Published private(set) var approximateLocation: CLLocation?
+    var approximateLocationPublisher: Published<CLLocation?>.Publisher {
+        return $approximateLocation
+    }
     
     let kMovementThreshold = 30.0
     
--- TrigpointsUITests/LocationAuthorizationUITests.swift
+++ TrigpointsUITests/LocationAuthorizationUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class TrigpointsUITests: XCTestCase {
+class LocationAuthorizationUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
